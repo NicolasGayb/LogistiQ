065ee07309e691566ed3db7d7c1e8ec6354e84e1
NicolasGayb <nicolasgbleal@gmail.com>
2025-09-08 20:54:55 -0300

    fix test config in init

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate  # <-- Importa Flask-Migrate
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 import secrets
@@ -10,18 +10,20 @@ import secrets
 # Instancia as extensões
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'auth.login'  # Define a rota padrão para login
+login_manager.login_view = 'auth.login'
 migrate = Migrate()
-mail = Mail()  # <-- Agora o mail é global, fora da função
+mail = Mail()
 load_dotenv()
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
     secret_key = os.environ.get("SECRET_KEY") or secrets.token_hex(32)
     app.secret_key = secret_key
+
+    # Config padrão (produção/dev)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     # Configuração do e-mail
     app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
@@ -30,9 +32,12 @@ def create_app():
     app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS', 'True') == 'True'
     app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 
-    
     # Configuração JWT
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+    # Se vier configuração de teste, sobrescreve
+    if test_config:
+        app.config.update(test_config)
 
     # Inicializa as extensões
     db.init_app(app)
@@ -40,14 +45,13 @@ def create_app():
     migrate.init_app(app, db)
     mail.init_app(app)
 
-    # Importa modelo de usuário para o carregamento de sessões
     from app.models import Usuario
 
     @login_manager.user_loader
     def load_user(user_id):
         return db.session.get(Usuario, int(user_id))
 
-    # Registra os blueprints de rotas
+    # Registra os blueprints
     from app.routes.web_routes import routes as routes_blueprint
     from app.routes.auth_routes import auth as auth_blueprint
     from app.routes.api_routes import api as api_blueprint
