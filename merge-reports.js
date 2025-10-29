bb99d042823dec3ec1ffa1c6b58c653292e799ff
NicolasGayb <nicolasgbleal@gmail.com>
2025-10-29 18:22:51 -0300

    ajuste CI/CD

---
+++ merge-reports.js
@@ -0,0 +1,9 @@
+// merge-reports.js
+const { merge } = require('mochawesome-merge');
+const fs = require('fs');
+
+(async () => {
+  const jsonReport = await merge({ files: ['cypress/reports/*.json'] });
+  fs.writeFileSync('cypress/reports/merged.json', JSON.stringify(jsonReport, null, 2));
+  console.log('✅ Relatórios mesclados com sucesso!');
+})();
