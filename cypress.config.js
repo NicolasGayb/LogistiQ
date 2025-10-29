d1f264f9e4e01d3a97bcf310734e789f97b754af
NicolasGayb <nicolasgbleal@gmail.com>
2025-10-29 17:16:48 -0300

    ajuste CI/CD

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,27 @@
 const { defineConfig } = require('cypress');
-const webpack = require('webpack');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
+    reportDir: 'cypress/reports',  // Diretório único e fixo
     charts: true,
     overwrite: false,
     html: true,
     json: true,
-    reportDir: '/cypress/reports',
+    embeddedScreenshots: true,
+    inlineAssets: true
   },
   e2e: {
     baseUrl: 'http://localhost:5000',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+
+      // 🔹 apenas registra o evento, sem forçar merge automático
+      on('after:run', () => {
+        console.log('📊 Cypress executado — JSONs prontos para merge.');
+      });
+
       return config;
     },
   },
